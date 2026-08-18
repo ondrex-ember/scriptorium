25c0a8934895dbbc82cafe91d251be718a6fd40a
Ondrex <ember.pan@gmail.com>
2026-08-18 20:22:51 +0200

    Add researcher role to dormitorium data

--- scriptorium/src/data/dormitorium.js
+++ scriptorium/src/data/dormitorium.js
@@ -220,6 +220,7 @@ const DormitoriumSpecializationDB = {
     apiarium: { name: 'Včelař',     name_en: 'Beekeeper',    icon: '🐝' },
     piscina:  { name: 'Rybář',      name_en: 'Fisherman',    icon: '🐟' },
     athanor:  { name: 'Alchymista', name_en: 'Alchemist',    icon: '⚗️' },
+    athanor_research: { name: 'Badatel', name_en: 'Researcher', icon: '🎡' },
     columbarium: { name: 'Columbarius', name_en: 'Columbarius', icon: '🕊️' },
     scriptorium: { name: 'Skriptor', name_en: 'Scriptor',    icon: '📜' },
     kostel:   { name: 'Kostelník',  name_en: 'Sacristan',    icon: '⛪' },
